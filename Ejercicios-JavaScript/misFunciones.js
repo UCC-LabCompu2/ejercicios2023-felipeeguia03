ebfa400c921af5598d038fd654451e1caa152482
felipeeguia03 <felipeeguia03@gmail.com>
2023-06-27 21:43:31 -0300

    probando webstorm

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,16 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cambiarUnidades(id, value ){
+    if (isNaN(valor)){
+        alert("se ingreso un valor invalido");
+    }
+}
+
+function dibujarCirCuad(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    ctx.fillstyle= "#333899";
+    ctx.fillRect(0,0,40,40);
+
+}
