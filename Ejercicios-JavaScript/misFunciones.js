dca6067b642ff1503e7fa5730465a04284578576
felipeeguia03 <felipeeguia03@gmail.com>
2023-06-28 12:51:54 -0300

    se realizaron ejercicios de javascript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,10 +5,31 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
-function cambiarUnidades(id, value ){
+function cambiarUnidades(id, valor ){
     if (isNaN(valor)){
         alert("se ingreso un valor invalido");
+        document.lasunidades.unid_metro.value = "";
+        document.lasunidades.unid_pulgada.value = "";
+        document.lasunidades.unid_pie.value = "";
+        document.lasunidades.unid_yarda.value = "";
+    }else if (id=="metro"){
+        document.lasunidades.unid_pulgada.value = 39.3701 * valor;
+        document.lasunidades.unid_pie.value = 3.2804 * valor;
+        document.lasunidades.unid_yarda.value = 1.0936 * valor;
+    }else if (id=="pulgada"){
+        document.lasunidades.unid_metro.value = 0.0254 * valor;
+        document.lasunidades.unid_pie.value = 0.0833333 * valor;
+        document.lasunidades.unid_yarda.value = 0.02777 * valor;
+    }else if (id=="pie"){
+        document.lasunidades.unid_metro.value = 0.3048 * valor;
+        document.lasunidades.unid_pulgada.value = 12 * valor;
+        document.lasunidades.unid_yarda.value = 0.3333 * valor;
+    }else if (id=="yarda"){
+        document.lasunidades.unid_metro.value = 0.9144 * valor;
+        document.lasunidades.unid_pie.value = 3 * valor;
+        document.lasunidades.unid_pulgada.value = 36 * valor;
     }
+
 }
 
 function dibujarCirCuad(){
