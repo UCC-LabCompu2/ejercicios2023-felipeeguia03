93f175f017ad987f27883071cbcdae533427fefc
felipeeguia03 <felipeeguia03@gmail.com>
2023-06-28 15:16:55 -0300

    se realizo ejercicio de ocultar div

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,32 +5,52 @@
  * @param {number} valor - El valor de los inputs de metros, yardas, pies o pulgdas
  * @return
  */
-function cambiarUnidades(id, valor ){
-    if (isNaN(valor)){
-        alert("se ingreso un valor invalido");
-        document.lasunidades.unid_metro.value = "";
-        document.lasunidades.unid_pulgada.value = "";
-        document.lasunidades.unid_pie.value = "";
-        document.lasunidades.unid_yarda.value = "";
-    }else if (id=="metro"){
-        document.lasunidades.unid_pulgada.value = 39.3701 * valor;
-        document.lasunidades.unid_pie.value = 3.2804 * valor;
-        document.lasunidades.unid_yarda.value = 1.0936 * valor;
-    }else if (id=="pulgada"){
-        document.lasunidades.unid_metro.value = 0.0254 * valor;
-        document.lasunidades.unid_pie.value = 0.0833333 * valor;
-        document.lasunidades.unid_yarda.value = 0.02777 * valor;
-    }else if (id=="pie"){
-        document.lasunidades.unid_metro.value = 0.3048 * valor;
-        document.lasunidades.unid_pulgada.value = 12 * valor;
-        document.lasunidades.unid_yarda.value = 0.3333 * valor;
-    }else if (id=="yarda"){
-        document.lasunidades.unid_metro.value = 0.9144 * valor;
-        document.lasunidades.unid_pie.value = 3 * valor;
-        document.lasunidades.unid_pulgada.value = 36 * valor;
+ cambiarUnidades = (id, valor ) => {
+    let met, pie, pul, yar;
+    if(valor.includes(",")) {
+        valor = valor.replace(",", ".")
     }
-
+    if(isNaN(valor)){
+        alert("El valor ingresado es incorrecto");
+        met = "";
+        pul = "";
+        pie = "";
+        yar = "";
+    }else if(id==="metro"){
+        met = valor;
+        pul = valor*39.3701;
+        pie = valor*3.28084;
+        yar = valor*1.09361;
+    }else if(id==="pulgada"){
+        met = 0.0254 * valor;
+        pie = 0.0833333 * valor;
+        yar = 0.02777 * valor;
+        pul = valor;
+    }
+    else if(id==="pie"){
+        met = 0.3048 * valor;
+        pul = 12 * valor;
+        yar = 0.3333 * valor;
+        pie = valor;
+    }else if(id==="yarda"){
+        met = 0.9144 * valor;
+        pie  = 3 * valor;
+        pul = 36 * valor;
+        yar = valor;
+    }
+    document.lasUnidades.unid_metro.value = Math.round(met*100)/100;
+    document.lasUnidades.unid_pulgada.value = Math.round(pul*100)/100;
+    document.lasUnidades.unid_pie.value = Math.round(pie);
+    document.lasUnidades.unid_yarda.value = Math.round(yar);
 }
+function mostrar_ocultar(valorMO){
+    if(valorMO=="val_mostrar"){
+        document.getElementById("divMo").style.display = 'block';
+    } else if(valorMO=="val_ocultar"){
+        document.getElementById("divMo").style.display = 'none';
+    }
+}
+
 function convertirGR(id){
     var grad, rad;
     if (id=="grados"){
